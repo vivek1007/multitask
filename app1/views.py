66efec6a7d3362e6dab011d2518ed50691b2f4ef
vivek <tomarvvk1007@gmail.com>
2020-07-25 11:22:30 +0530

    add file

---
+++ app1/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def show(request):
+    return HttpResponse("hello sir")
+
+def show_temp(request):
+    return render(request,'app1/home.html')
+
+
+def analyze(request):
+#Get the text
+    djtext = request.GET.get('text', 'default')
+
+    # Check checkbox values
+    removepunc = request.GET.get('removepunc', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremover = request.GET.get('newlineremover', 'off')
+    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+
+    #Check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'app1/analyze.html', params)
+
+    if(fullcaps=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'app1/analyze.html', params)
+
+    if(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'app1/analyze.html', params)
+
+    if (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n":
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'app1/analyze.html', params)
+    else:
+        return HttpResponse("Error")
+
+def capfirst(request):
+    return HttpResponse("Cap first")
+
+def newlineremove(request):
+    return HttpResponse("new line remove")
+
+def spaceremove():
+    return HttpResponse("remove space")
+
+def charcount():
+    return HttpResponse("char count")
